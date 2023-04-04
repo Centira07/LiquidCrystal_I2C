fec7f69c6e98128383e64d3dd4611f8d17a40619
Centira07 <123511269+Centira07@users.noreply.github.com>
2023-04-03 20:33:19 -0400

    Update hd44780.h

--- LIB/hd44780.h
+++ LIB/hd44780.h
@@ -47,7 +47,31 @@
 
 class LiquidCrystal_I2C : public Print {
 public:
-  LiquidCrystal_I2C();
+  LiquidCrystal_I2C(uint8_t addr, uint8_t cols, uint8_t rows);
+  void begin(uint8_t cols, uint8_t rows, uint8_t charsize = LCD_5x8DOTS);
+  void clear();
+  void home();
+  void noDisplay();
+  void display();
+  void noBlink();
+  void blink();
+  void noCursor();
+  void cursor();
+  void scrollDisplayLeft();
+  void scrollDisplayRight();
+  void printLeft();
+  void printRight();
+  void leftToRight();
+  void rightToLeft();
+  void shiftIncrement();
+  void shiftDecrement();
+  void noBacklight();
+  void backlight();
+  void autoscroll();
+  void noAutoscroll(); 
+  void createChar(uint8_t, uint8_t[]);
+  void createChar(uint8_t location, const char *charmap);
+  void setCursor(uint8_t x, uint8_t y);
   
 };
 
