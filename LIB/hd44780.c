1c3b3c5b51502597ba133e37fa72be08553417b8
Centira07 <123511269+Centira07@users.noreply.github.com>
2023-04-04 16:06:14 -0400

    Update hd44780.c

--- LIB/hd44780.c
+++ LIB/hd44780.c
@@ -3,3 +3,12 @@
 #include "Arduino.h"
 #include <Wire.h>
 
+LiquidCrystal_I2C::LiquidCrystal_I2C(uint8_t lcd_addr, uint8_t lcd_cols, uint8_t lcd_rows, uint8_t charsize)
+{
+	_addr = lcd_addr;
+	_cols = lcd_cols;
+	_rows = lcd_rows;
+	_charsize = charsize;
+	_backlightval = LCD_BACKLIGHT;
+}
+
