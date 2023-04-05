c68c140cc943d98e5d6a1a49b21b1aecdd9a2edc
Centira07 <123511269+Centira07@users.noreply.github.com>
2023-04-05 18:20:41 -0400

    Update hd44780.c

--- LIB/hd44780.c
+++ LIB/hd44780.c
@@ -1,262 +1,110 @@
 #include "hd44780.h"
-#include <inttypes.h>
-#include "Arduino.h"
-#include <Wire.h>
+#include "i2c_master.h"
+#include <avr/io.h>
+#include <util/delay.h>
 
-LiquidCrystal_I2C::LiquidCrystal_I2C(uint8_t lcd_addr, uint8_t lcd_cols, uint8_t lcd_rows, uint8_t charsize)
+LiquidCrystalDevice init(uint8_t address, uint8_t columns, uint8_t rows, uint8_t dotSize)
 {
-	_addr = lcd_addr;
-	_cols = lcd_cols;
-	_rows = lcd_rows;
-	_charsize = charsize;
-	_backlightval = LCD_BACKLIGHT;
-}
+	LiquidCrystalDevice device;
 
-void LiquidCrystal_I2C::begin() {
-	Wire.begin();
-	_displayfunction = LCD_4BITMODE | LCD_1LINE | LCD_5x8DOTS;
+	device.Address = address;
+	device.Columns = columns;
+	device.Rows = rows;
+	device.Backlight = LCD_NOBACKLIGHT;
+	device.DisplayFunction = LCD_4BITMODE | LCD_1LINE | dotSize;
+	device.DisplayControl = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF;
 
-	if (_rows > 1) {
-		_displayfunction |= LCD_2LINE;
+	// Initialize to default text direction (for roman languages)
+	device.DisplayMode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT;
+
+	if (rows > 1) {
+		device.DisplayFunction |= LCD_2LINE;
 	}
 
 	// for some 1 line displays you can select a 10 pixel high font
-	if ((_charsize != 0) && (_rows == 1)) {
-		_displayfunction |= LCD_5x10DOTS;
+	if ((dotSize != 0) && (rows == 1)) {
+		device.DisplayFunction |= LCD_5x10DOTS;
 	}
 
 	// SEE PAGE 45/46 FOR INITIALIZATION SPECIFICATION!
 	// according to datasheet, we need at least 40ms after power rises above 2.7V
 	// before sending commands. Arduino can turn on way befer 4.5V so we'll wait 50
-	delay(50);
-
+	_delay_ms(100);
+	
 	// Now we pull both RS and R/W low to begin commands
-	expanderWrite(_backlightval);	// reset expanderand turn backlight off (Bit 8 =1)
-	delay(1000);
+	transmitI2C(&device, LCD_NOBACKLIGHT);	// reset expanderand turn backlight off (Bit 8 =1)
+	_delay_ms(1000);
 
 	//put the LCD into 4 bit mode
 	// this is according to the hitachi HD44780 datasheet
 	// figure 24, pg 46
-
+	
 	// we start in 8bit mode, try to set 4 bit mode
-	write4bits(0x03 << 4);
-	delayMicroseconds(4500); // wait min 4.1ms
-
+	transmitI2C(&device, 0x03 << 4);
+	_delay_us(4500); // wait min 4.1ms
+	
 	// second try
-	write4bits(0x03 << 4);
-	delayMicroseconds(4500); // wait min 4.1ms
-
+	writeDevice4Bits(&device, 0x03 << 4);
+	_delay_us(4500); // wait min 4.1ms
+	
 	// third go!
-	write4bits(0x03 << 4);
-	delayMicroseconds(150);
-
+	writeDevice4Bits(&device, 0x03 << 4);
+	_delay_us(150); // wait min 150 mics
+	
 	// finally, set to 4-bit interface
-	write4bits(0x02 << 4);
+	writeDevice4Bits(&device, 0x02 << 4);
 
 	// set # lines, font size, etc.
-	command(LCD_FUNCTIONSET | _displayfunction);
-
-	// turn the display on with no cursor or blinking default
-	_displaycontrol = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF;
-	display();
-
-	// clear it off
-	clear();
-
-	// Initialize to default text direction (for roman languages)
-	_displaymode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT;
-
-	// set the entry mode
-	command(LCD_ENTRYMODESET | _displaymode);
-
-	home();
-}
-
-void LiquidCrystal_I2C::oled_init(){
-  _oled = true;
-	init_priv();
-}
-
-void LiquidCrystal_I2C::init(){
-	init_priv();
-}
-
-void LiquidCrystal_I2C::init_priv()
-{
-	Wire.begin();
-	_displayfunction = LCD_4BITMODE | LCD_1LINE | LCD_5x8DOTS;
-	begin(_cols, _rows);  
-}
-
-void LiquidCrystal_I2C::clear(){
-	command(LCD_CLEARDISPLAY);// clear display, set cursor position to zero
-	delayMicroseconds(2000);  // this command takes a long time!
-  if (_oled) setCursor(0,0);
-}
-
-void LiquidCrystal_I2C::home(){
-	command(LCD_RETURNHOME);  // set cursor position to zero
-	delayMicroseconds(2000);  // this command takes a long time!
-}
-
-void LiquidCrystal_I2C::setCursor(uint8_t col, uint8_t row){
-	int row_offsets[] = { 0x00, 0x40, 0x14, 0x54 };
-	if ( row > _numlines ) {
-		row = _numlines-1;    // we count rows starting w/0
-	}
-	command(LCD_SETDDRAMADDR | (col + row_offsets[row]));
-}
-
-void LiquidCrystal_I2C::noDisplay() {
-	_displaycontrol &= ~LCD_DISPLAYON;
-	command(LCD_DISPLAYCONTROL | _displaycontrol);
-}
-
-void LiquidCrystal_I2C::display() {
-	_displaycontrol |= LCD_DISPLAYON;
-	command(LCD_DISPLAYCONTROL | _displaycontrol);
-}
-
-void LiquidCrystal_I2C::noCursor() {
-	_displaycontrol &= ~LCD_CURSORON;
-	command(LCD_DISPLAYCONTROL | _displaycontrol);
-}
-
-void LiquidCrystal_I2C::cursor() {
-	_displaycontrol |= LCD_CURSORON;
-	command(LCD_DISPLAYCONTROL | _displaycontrol);
-}
-
-void LiquidCrystal_I2C::noBlink() {
-	_displaycontrol &= ~LCD_BLINKON;
-	command(LCD_DISPLAYCONTROL | _displaycontrol);
-}
-
-void LiquidCrystal_I2C::blink() {
-	_displaycontrol |= LCD_BLINKON;
-	command(LCD_DISPLAYCONTROL | _displaycontrol);
-}
-
-void LiquidCrystal_I2C::scrollDisplayLeft(void) {
-	command(LCD_CURSORSHIFT | LCD_DISPLAYMOVE | LCD_MOVELEFT);
-}
-
-void LiquidCrystal_I2C::scrollDisplayRight(void) {
-	command(LCD_CURSORSHIFT | LCD_DISPLAYMOVE | LCD_MOVERIGHT);
-}
-
-// This is for text that flows Left to Right
-void LiquidCrystal_I2C::leftToRight(void) {
-	_displaymode |= LCD_ENTRYLEFT;
-	command(LCD_ENTRYMODESET | _displaymode);
-}
-
-// This is for text that flows Right to Left
-void LiquidCrystal_I2C::rightToLeft(void) {
-	_displaymode &= ~LCD_ENTRYLEFT;
-	command(LCD_ENTRYMODESET | _displaymode);
-}
-
-void LiquidCrystal_I2C::autoscroll(void) {
-	_displaymode |= LCD_ENTRYSHIFTINCREMENT;
-	command(LCD_ENTRYMODESET | _displaymode);
-}
-
-void LiquidCrystal_I2C::noAutoscroll(void) {
-	_displaymode &= ~LCD_ENTRYSHIFTINCREMENT;
-	command(LCD_ENTRYMODESET | _displaymode);
-}
-
-// Allows us to fill the first 8 CGRAM locations
-// with custom characters
-void LiquidCrystal_I2C::createChar(uint8_t location, uint8_t charmap[]) {
-	location &= 0x7; // we only have 8 locations 0-7
-	command(LCD_SETCGRAMADDR | (location << 3));
-	for (int i=0; i<8; i++) {
-		write(charmap[i]);
-	}
-}
-
-void LiquidCrystal_I2C::noBacklight(void) {
-	_backlightval=LCD_NOBACKLIGHT;
-	expanderWrite(0);
-}
-
-void LiquidCrystal_I2C::backlight(void) {
-	_backlightval=LCD_BACKLIGHT;
-	expanderWrite(0);
-}
-
-bool LiquidCrystal_I2C::getBacklight() {
-  return _backlightval == LCD_BACKLIGHT;
-}
-
-inline void LiquidCrystal_I2C::command(uint8_t value) {
-	send(value, 0);
-}
-
-inline size_t LiquidCrystal_I2C::write(uint8_t value) {
-	send(value, Rs);
-	return 1;
-}
-
-// write either command or data
-void LiquidCrystal_I2C::send(uint8_t value, uint8_t mode) {
-	uint8_t highnib=value&0xf0;
-	uint8_t lownib=(value<<4)&0xf0;
-       write4bits((highnib)|mode);
-	write4bits((lownib)|mode); 
-}
-
-void LiquidCrystal_I2C::write4bits(uint8_t value) {
-	expanderWrite(value);
-	pulseEnable(value);
-}
-
-void LiquidCrystal_I2C::expanderWrite(uint8_t _data){
-	Wire.beginTransmission(_addr);
-	Wire.write((int)(_data) | _backlightval);
-	Wire.endTransmission();
-}
-
-void LiquidCrystal_I2C::pulseEnable(uint8_t _data){
-	expanderWrite(_data | En);	// En high
-	delayMicroseconds(1);		// enable pulse must be >450ns
+	sendCommand(&device, LCD_FUNCTIONSET | device.DisplayFunction);
 	
-	expanderWrite(_data & ~En);	// En low
-	delayMicroseconds(50);		// commands need > 37us to settle
-} 
+	// turn the display on with no cursor or blinking default
+	turnOnDisplay(&device);
+	
+	// clear it off
+	clear(&device);
+	
+	// set the entry mode
+	sendCommand(&device, LCD_ENTRYMODESET | device.DisplayMode);
+	
+	returnHome(&device);
 
-void LiquidCrystal_I2C::cursor_on(){
-	cursor();
-}
+	return device;
+};
 
-void LiquidCrystal_I2C::cursor_off(){
-	noCursor();
-}
+void print(struct LiquidCrystalDevice* device, char * value)
+{
+	char letter = *value;
 
-void LiquidCrystal_I2C::blink_on(){
-	blink();
-}
-
-void LiquidCrystal_I2C::blink_off(){
-	noBlink();
-}
-
-void LiquidCrystal_I2C::load_custom_character(uint8_t char_num, uint8_t *rows){
-		createChar(char_num, rows);
-}
-
-void LiquidCrystal_I2C::setBacklight(uint8_t new_val){
-	if(new_val){
-		backlight();		// turn backlight on
-	}else{
-		noBacklight();		// turn backlight off
+	while(letter != 0x00)
+	{
+		writeDeviceByte(device, letter, LCD_REGISTER_SELECT_BIT);
+		letter = *(++value);
 	}
+};
+
+void turnOnBacklight(struct LiquidCrystalDevice* device)
+{
+	device->Backlight = LCD_BACKLIGHT;
+	transmitI2C(device, 0);
 }
 
-void LiquidCrystal_I2C::printstr(const char c[]){
-	//This function is not identical to the function used for "real" I2C displays
-	//it's here so the user sketch doesn't have to be changed 
-	print(c);
+void turnOffBacklight(struct LiquidCrystalDevice* device)
+{
+	device->Backlight = LCD_NOBACKLIGHT;
+	transmitI2C(device, 0);
+}
+
+void clear(LiquidCrystalDevice* device)
+{
+	sendCommand(device, LCD_CLEARDISPLAY); // clear display, set cursor position to zero
+	_delay_us(2000);  // this command takes a long time!
+
+	setCursor(device, 0, 0);
+}
+
+void setCursor(LiquidCrystalDevice* device, uint8_t row, uint8_t column)
+{
+	uint8_t row_offsets[] = { 0x00, 0x40, 0x14, 0x54 };
+
+	sendCommand(device, LCD_SETDDRAMADDR | (column + row_offsets[row]));
 }
